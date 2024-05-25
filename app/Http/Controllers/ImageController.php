2f80957b371c55fc72c9f8cc04294400bb0cc955
Margarida Peixoto <margaridapeixoto1996@gmail.com>
2024-05-25 22:26:00 +0100

    images upload not finished

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    //
+    /* public function upload(Request $request)
+    {
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $imageName = time().'.'.$request->image->extension();  
+   
+        $request->image->move(public_path('images'), $imageName);
+
+        // Salve o caminho da imagem no banco de dados, por exemplo:
+        // Image::create(['path' => '/images/'.$imageName]);
+
+        return redirect()->back()->with('success', 'Image uploaded successfully.');
+    } */
 }
